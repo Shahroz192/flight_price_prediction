2ebabf61b95a59b39b94a9ee5723e9de9cbf2a3f
shahroz <buttshahroz192@gmail.com>
2024-05-11 15:42:12 +0500

    first commit

---
+++ src/data/make_dataset.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import logging
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def stop(df):
+    """
+    Maps the 'Total_Stops' column to numerical values.
+    """
+    df["Total_Stops"] = df["Total_Stops"].map(
+        {
+            "non-stop": 0,
+            "1 stop": 1,
+            "2 stops": 2,
+            "3 stops": 3,
+            "4 stops": 4,
+            "nan": np.nan,
+        }
+    )
+    df["Total_Stops"] = df["Total_Stops"].astype(int)
+    return df
+
+
+def duration(df):
+    """
+    Converts the 'Duration' column to numeric values (in minutes).
+    """
+    df["Duration"] = (
+        df["Duration"]
+        .str.replace("h", "*60")
+        .str.replace(" ", "+")
+        .str.replace("m", "*1")
+        .apply(eval)
+    )
+    df["Duration"] = df["Duration"].astype(int)
+    return df
+
+
+def to_datetime(df):
+    """
+    Converts the 'Date_of_Journey', 'Dep_Time', and 'Arrival_Time' columns to datetime format.
+    """
+    df["Date_of_Journey"] = pd.to_datetime(df["Date_of_Journey"])
+    df["Dep_Time"] = pd.to_datetime(df["Dep_Time"])
+    df["Arrival_Time"] = pd.to_datetime(df["Arrival_Time"])
+    return df
+
+
+def cleaning(df):
+    """
+    Performs data cleaning and preprocessing on the input DataFrame.
+    """
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.drop(["Route"], axis=1, inplace=True)
+    duration(df)
+    stop(df)
+    to_datetime(df)
+    return df
+
+
+def main():
+    """
+    Runs data processing scripts to turn raw data from (../raw) into cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info("Making final data set from raw data")
+
+    # Hardcoded file paths
+    input_filepath = Path("d:/flight_price_prediction/data/raw/flight_price.csv")
+
+    try:
+        df = pd.read_csv(input_filepath)
+        cleaned_df = cleaning(df)
+        train, test = train_test_split(cleaned_df, test_size=0.2, random_state=42)
+        train.to_csv("d:/flight_price_prediction/data/processed/train.csv", index=False)
+        test.to_csv("d:/flight_price_prediction/data/processed/test.csv", index=False)
+
+        logger.info("Processed data saved.")
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    load_dotenv(find_dotenv())
+    main()
