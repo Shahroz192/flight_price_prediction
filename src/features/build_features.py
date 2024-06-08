5fca22e3959dac7d0b9c6d284994ff2c0a1352a5
shahroz <buttshahroz192@gmail.com>
2024-06-08 16:52:15 +0500

    created features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -40,6 +40,7 @@ def main():
 
     return
 
+
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
